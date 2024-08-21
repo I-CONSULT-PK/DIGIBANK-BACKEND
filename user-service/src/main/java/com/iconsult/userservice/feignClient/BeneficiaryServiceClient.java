659ea35606be2b2413783de497862f15dc082301
haseeb413 <Haseebullah.11356@iqra.edu.pk>
2024-08-21 16:26:08 +0500

    Bene Feign Client

---
+++ user-service/src/main/java/com/iconsult/userservice/feignClient/BeneficiaryServiceClient.java
@@ -0,0 +1,14 @@
+package com.iconsult.userservice.feignClient;
+
+import com.zanbeel.customUtility.model.CustomResponseEntity;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "add-payeee", url = "http://localhost:8090/v1/beneficiary") // Replace with the actual service name or URL
+public interface BeneficiaryServiceClient {
+
+    @PostMapping("/addTransferAmount")
+    public CustomResponseEntity addTransferAmountToBene (@RequestParam String accountNumber , @RequestParam String transferAmount, @RequestParam Long customerId);
+}
