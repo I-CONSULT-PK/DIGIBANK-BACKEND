5c955e1018aa8dcc73bceb3a41577bdda83928c9
Bilal Ali <bilalbhutto121@gmail.com>
2024-09-09 11:47:09 +0500

    topup microservice created.

---
+++ topup-service/src/main/java/com/iconsult/topup/TopupServiceApplication.java
@@ -0,0 +1,14 @@
+package com.iconsult.topup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopupServiceApplication.class, args);
+		System.out.println("_________________________Bilal ________________");
+	}
+
+}
---
+++ topup-service/src/test/java/com/iconsult/topup/TopupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iconsult.topup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
