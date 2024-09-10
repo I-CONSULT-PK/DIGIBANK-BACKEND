0837c7d44aafd9263554fddfb7b6c652e6584b89
Jahangeer Kalwar <ahmedkalwar38@gmail.com>
2024-09-10 12:30:27 +0500

    method updated in TopUpCustomerRepository.

--- topup-service/src/main/java/com/iconsult/topup/repo/TopUpCustomerRepository.java
+++ topup-service/src/main/java/com/iconsult/topup/repo/TopUpCustomerRepository.java
@@ -3,12 +3,16 @@ package com.iconsult.topup.repo;
 import com.iconsult.topup.constants.CarrierType;
 import com.iconsult.topup.model.entity.TopUpCustomer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface TopUpCustomerRepository  extends JpaRepository<TopUpCustomer, Long> {
 
     Optional<TopUpCustomer> findByMobileNumber(String mobileNumber);
-    Optional<TopUpCustomer> findByMobileNumberAndCarrierType(String mobileNumber, CarrierType carrierType);
+
+    @Query("SELECT t FROM TopUpCustomer t WHERE t.mobileNumber = :mobileNumber AND t.carrierType = :carrierType")
+    Optional<TopUpCustomer> findByMobileNumberAndCarrierType(@Param("mobileNumber") String mobileNumber, @Param("carrierType") CarrierType carrierType);
 
 }
