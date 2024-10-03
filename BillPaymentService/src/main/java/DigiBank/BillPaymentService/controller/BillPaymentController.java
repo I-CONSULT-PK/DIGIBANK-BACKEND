234ec6d70e0c5698d074b1d110674c9fd49aa037
Jahangeer Kalwar <ahmedkalwar38@gmail.com>
2024-10-03 10:20:27 +0500

    billpayment service registered with eureka

--- BillPaymentService/src/main/java/DigiBank/BillPaymentService/controller/BillPaymentController.java
+++ BillPaymentService/src/main/java/DigiBank/BillPaymentService/controller/BillPaymentController.java
@@ -1,7 +1,9 @@
 package DigiBank.BillPaymentService.controller;
 
 import DigiBank.BillPaymentService.constants.CustomResponseEntity;
+import DigiBank.BillPaymentService.constants.UtilityType;
 import DigiBank.BillPaymentService.model.dto.request.BillDto;
+import DigiBank.BillPaymentService.model.dto.request.BillerDtoRequest;
 import DigiBank.BillPaymentService.service.BillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,10 +22,25 @@ public class BillPaymentController {
         return billService.getBillDetailsByConsumerNumber(consumerNumber, serviceCode, utilityType);
 
     }
-
     @PostMapping("/createBill")
     public CustomResponseEntity createBill(@RequestBody BillDto billDto) {
         return  billService.createBill(billDto);
+    }
+
+    @PostMapping("/addBiller")
+    public CustomResponseEntity addBiller(@RequestBody BillerDtoRequest request) {
+        return billService.addBiller(request);
+    }
+
+    @GetMapping("/getUtilityTypes")
+    public CustomResponseEntity getAllUtilityTypes() {
+        return this.billService.getAllUtilityTypes();
+    }
+
+
+    @GetMapping("/getBillers")
+    public CustomResponseEntity getBillers(@RequestParam("utilityType") UtilityType utilityType) {
+        return this.billService.getAllBillers(utilityType);
     }
 
 }
--- BillPaymentService/src/main/java/DigiBank/BillPaymentService/controller/BillerController.java
+++ BillPaymentService/src/main/java/DigiBank/BillPaymentService/controller/BillerController.java
@@ -1,32 +1,11 @@
 package DigiBank.BillPaymentService.controller;
 
-import DigiBank.BillPaymentService.constants.CustomResponseEntity;
-import DigiBank.BillPaymentService.constants.UtilityType;
-import DigiBank.BillPaymentService.model.dto.request.BillerDtoRequest;
-import DigiBank.BillPaymentService.service.BillService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/billpayment")
 public class BillerController {
 
-    @Autowired
-    private BillService service;
 
-    @PostMapping("/addBiller")
-    public CustomResponseEntity addBiller(@RequestBody BillerDtoRequest request) {
-        return service.addBiller(request);
-    }
-
-    @GetMapping("/getUtilityTypes")
-    public CustomResponseEntity getAllUtilityTypes() {
-        return this.service.getAllUtilityTypes();
-    }
-
-
-    @GetMapping("/getBillers")
-    public CustomResponseEntity getBillers(@RequestParam("utilityType") UtilityType utilityType) {
-        return this.service.getAllBillers(utilityType);
-    }
 }
